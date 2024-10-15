afffca5f435fa38b84bd6e568243ff78b78bba73
Naimur Rahman Naim <naimekattor@gmail.com>
2024-10-15 14:10:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Map Method</h1>
+      <ul className="list-group">
+        <li className="list-group-item">a item</li>
+        <li className="list-group-item">b item</li>
+        <li className="list-group-item">c item</li>
+        <li className="list-group-item">d item</li>
+        <li className="list-group-item">e item</li>
+      </ul>
+    </div>
+  );
+};
+export default App;
